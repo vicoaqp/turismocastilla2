24486c54707bc6130a2ee5269ebaa4ef90afe799
vicoaqp <victordan2@gmail.com>
2022-02-15 19:49:53 -0500

    UI1

---
+++ app/src/main/java/com/turismo/castilla/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.turismo.castilla
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val boton1=findViewById<Button>(R.id.button_princ)
+
+        boton1.setOnClickListener {
+            val lanzar= Intent(this, MenuDistritos::class.java)
+            startActivity(lanzar)
+        }
+
+    }
+}
